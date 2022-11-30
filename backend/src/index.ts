4c087864edc2dda9c5e2e2c355017fdf6864b73b
skhe55 <flashback2019@mail.ru>
2022-11-30 16:45:48 +0700

    added x-token header to cors & new controller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,12 +2,14 @@ import App from './app';
 import AuthController from './controllers/auth/auth.controller';
 import OrganizationsController from './controllers/organizations/organizations.controller';
 import RoomsController from './controllers/rooms/rooms.controller';
+import UsersController from './controllers/users/users.controller';
 
 const app = new App(
     [
         new OrganizationsController(),
         new RoomsController(),
-        new AuthController()
+        new AuthController(),
+        new UsersController()
     ]
 );
 
--- backend/src/interfaces/auth.interface.ts
+++ backend/src/interfaces/auth.interface.ts
@@ -2,3 +2,10 @@ export interface AuthDto {
     login: string;
     password: string;
 }
+
+export interface JwtDecodedPayload {
+    login: string;
+    password: string;
+    iat: number;
+    exp: number;
+}
